73fdc5af706b6bde38a650f5aa50d7bfa1f45c09
PawelKrzyzanowski <p.a.krzyzanowski@gmail.com>
2021-09-09 18:04:46 +0200

    ADD examples of usege for: Config Class, DB Class, connection check

--- index.php
+++ index.php
@@ -1,8 +1,24 @@
 <?php
     require_once('core/init.php');
-    //EXAMPLE OF USE Config CLASS
-    echo Config::get('mysql/host'); // 127.0.0.1
-    //echo Config::get('mysql/db'); 
-    //echo Config::get('session/session_name');
-    //echo Config::get('remember/cookie_expiry');
+    /* Eample of Config class usage */
+    echo '<br>host...........'.Config::get('mysql/host'); // 127.0.0.1
+    echo '<br>dbName.........'.Config::get('mysql/db'); 
+	echo '<br>cookieExpiry...'.Config::get('remember/cookie_expiry');
+    echo '<br>sessionName....'.Config::get('session/session_name');
+	/* Example of DB class usage
+	before getInstance declaration: when __construct was public, multiconnections where avialable 
+		$db = new DB();
+	after getInstance() declaration, __construct = DB() is private method of DB called by getInstance and PDO cretion looks like:*/
+	DB::getInstance();
+	/*
+		$users = DB::getInstance()->query("SELECT userName FROM users");
+		$users2 = DB::getInstance()->get('users', array('username','=','Alex'));
+		if($users->count)
+		{
+			foreach($users as $user)
+			{
+				echo $user->$userName;
+			}	
+		}
+	*/
 ?>
